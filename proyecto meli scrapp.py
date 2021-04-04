e6d3383b73ddbdc98eedd486ba8d0f4b585e0e6b
lartweib <a.federicodaniel@gmail.com>
2021-04-03 23:01:57 -0300

    Se agrego filtro que machea busqueda con titulos para traer solo los relacionados a la busqueda

--- proyecto meli scrapp.py
+++ proyecto meli scrapp.py
@@ -7,6 +7,7 @@ busqueda = input('Especifique que desea buscar en ML: ').lower()
 #normalizar busquedas para insertar en el link
 busqueda_ml = busqueda.replace(' ', '-')
 busqueda_ml = busqueda_ml.replace('+','þ')
+busqueda_split = busqueda.split(' ')
 
 #generacion del link de busqueda de ml
 link_ml = 'https://listado.mercadolibre.com.ar/'+busqueda_ml+'_OrderId_PRICE'
@@ -33,55 +34,65 @@ for publicacion in range(len(publicaciones)):
         #Extraer el titulo de la publicacion
         titulo = publicaciones[publicacion].div.a['title']
         titulo = titulo.lower()
-              
-        #normalizo las , que pudiera haber en el titulo
-        titulo = titulo.replace(',', ' ')
         
-        #Extraer el link de la publicacion
-        link = publicaciones[publicacion].div.a['href']
-                    
-        #Extraer el precio de la publicacion            
-        precio_busq = str(publicaciones[publicacion].find('span', class_='price-tag-fraction'))
-        pos = precio_busq.find('">')
-        pos_final = precio_busq.find('</')
-        precio = (precio_busq[pos+2:pos_final])
-        precio = precio.replace('.','')
-        precio = str(precio)
+        #Corroborar el titulo de la publicacion coincida con la busqueda
+        contador = 0
+        for i in range(len(busqueda_split)):
+            if re.search(busqueda_split[i], titulo):
+                contador += 1
+                continue
+            else: break
         
-        #Conexion con el link de la publicacion
-        response_prod = requests.get(link)
-        response_prod.encoding = 'utf-8'
-        archivo_html_prod = response_prod.text
-        dom_prod = BS(archivo_html_prod, features = 'html.parser')
-        
-        #Extraer cantidad disponible en la publicacion
-        cantidad_busq = str(dom_prod.find('span', class_='ui-pdp-buybox__quantity__available'))
-        pos = cantidad_busq.find('(')
-        pos_final = cantidad_busq.find('</')
-        cantidad = (cantidad_busq[pos+1:pos_final-1])
-        if re.search('No', cantidad): cantidad = '1'
-        else: continue 
-                    
-        #Extraer el nombre del vendedor    
-        vendedor_busq = str(dom_prod.find('span', class_='ui-pdp-color--BLUE'))
-        pos = vendedor_busq.find('">')
-        pos_final = vendedor_busq.find('</')
-        vendedor = (vendedor_busq[pos+2:pos_final])
-        
-        #Extraer el numero de ventas de la publicacion
-        ventas_busq = str(dom_prod.find_all('span', class_="ui-pdp-subtitle"))
-        pos = ventas_busq.find('| ')
-        pos_final = ventas_busq.find('</')
-        ventas = (ventas_busq[pos+3:pos_final])
-        if re.search('vendido', ventas):
-            continue
-        else: ventas = '0 vendidos'
+        if contador == len(busqueda_split):
+            #normalizo las , que pudiera haber en el titulo
+            titulo = titulo.replace(',', ' ')
+            
+            #Extraer el link de la publicacion
+            link = publicaciones[publicacion].div.a['href']
+                        
+            #Extraer el precio de la publicacion            
+            precio_busq = str(publicaciones[publicacion].find('span', class_='price-tag-fraction'))
+            pos = precio_busq.find('">')
+            pos_final = precio_busq.find('</')
+            precio = (precio_busq[pos+2:pos_final])
+            precio = precio.replace('.','')
+            precio = str(precio)
+            
+            #Conexion con el link de la publicacion
+            response_prod = requests.get(link)
+            response_prod.encoding = 'utf-8'
+            archivo_html_prod = response_prod.text
+            dom_prod = BS(archivo_html_prod, features = 'html.parser')
+            
+            #Extraer cantidad disponible en la publicacion
+            cantidad_busq = str(dom_prod.find('span', class_='ui-pdp-buybox__quantity__available'))
+            pos = cantidad_busq.find('(')
+            pos_final = cantidad_busq.find('</')
+            cantidad = (cantidad_busq[pos+1:pos_final-1])
+            if re.search('No', cantidad): cantidad = '1'
+            else: continue 
+                        
+            #Extraer el nombre del vendedor    
+            vendedor_busq = str(dom_prod.find('span', class_='ui-pdp-color--BLUE'))
+            pos = vendedor_busq.find('">')
+            pos_final = vendedor_busq.find('</')
+            vendedor = (vendedor_busq[pos+2:pos_final])
+            
+            #Extraer el numero de ventas de la publicacion
+            ventas_busq = str(dom_prod.find_all('span', class_="ui-pdp-subtitle"))
+            pos = ventas_busq.find('| ')
+            pos_final = ventas_busq.find('</')
+            ventas = (ventas_busq[pos+3:pos_final])
+            if re.search('vendido', ventas):
+                continue
+            else: ventas = '0 vendidos'
 
-        #escritura de datos en el csv por cada publicacion iterada
-        archivo_out.write(titulo+','+precio+','+cantidad+','+ventas+','+vendedor+','+link+'\n') 
+            #escritura de datos en el csv por cada publicacion iterada
+            archivo_out.write(titulo+','+precio+','+cantidad+','+ventas+','+vendedor+','+link+'\n') 
+            
+            #print(titulo+','+precio+','+cantidad+','+ventas+','+vendedor+','+link+'\n')
+        else: continue
         
-        #print(titulo+','+precio + '\n')
-         
     except: continue
     
     
